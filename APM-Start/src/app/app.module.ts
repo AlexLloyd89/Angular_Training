9a585441fda1e61207858d65ae26f54e09b1b2cf
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-16 11:29:13 -0400

    cli generation

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 import { StarComponent } from "./shared/star.component";
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent]
