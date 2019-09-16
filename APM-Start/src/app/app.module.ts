9a4e1d7793b3b79fc1ac746dc36e40d5e8b25cf3
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-16 11:49:49 -0400

    added routing

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 import { StarComponent } from "./shared/star.component";
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductDetailComponent } from "./products/product-detail/product-detail.component";
+import { WelcomeComponent } from "./home/welcome.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -14,9 +16,21 @@ import { ProductDetailComponent } from './products/product-detail/product-detail
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    WelcomeComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- APM-Start/src/app/products/product-detail/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail/product-detail.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "../product";
 
 @Component({
-  selector: 'pm-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"]
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle = "Product Detail";
+  product: IProduct;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
