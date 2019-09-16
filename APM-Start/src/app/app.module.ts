08b4e15c6ac74839dc82d0c99fa5c22d0e3a7dbd
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-16 09:09:28 -0400

    added some pipes

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
