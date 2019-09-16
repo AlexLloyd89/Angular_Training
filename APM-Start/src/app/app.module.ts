2685447ba6e12ea35dbae71dff0c77f3919d07f3
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-16 11:27:48 -0400

    http call

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
@@ -13,7 +14,7 @@ import { StarComponent } from "./shared/star.component";
     ConvertToSpacesPipe,
     StarComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: "pm-products",
@@ -11,6 +12,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string;
 
   _listFilter: string;
   get listFilter(): string {
@@ -24,63 +26,9 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png"
-    },
-    {
-      productId: 5,
-      productName: "Hammer",
-      productCode: "TBX-0048",
-      releaseDate: "May 21, 2019",
-      description: "Curved claw steel hammer",
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: "assets/images/hammer.png"
-    },
-    {
-      productId: 8,
-      productName: "Saw",
-      productCode: "TBX-0022",
-      releaseDate: "May 15, 2019",
-      description: "15-inch steel blade hand saw",
-      price: 11.55,
-      starRating: 3.7,
-      imageUrl: "assets/images/saw.png"
-    },
-    {
-      productId: 10,
-      productName: "Video Game Controller",
-      productCode: "GMG-0042",
-      releaseDate: "October 15, 2018",
-      description: "Standard two-button video game controller",
-      price: 35.95,
-      starRating: 4.6,
-      imageUrl: "assets/images/xbox-controller.png"
-    }
-  ];
+  products: IProduct[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = "cart";
-  }
+  constructor(private productService: ProductService) {}
 
   onRatingClicked(message: string): void {
     this.pageTitle = "Product List: " + message;
@@ -99,6 +47,11 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(`ng onit`);
+    this.productService.getProducts().subscribe({
+      next: products => {
+        (this.products = products), (this.filteredProducts = this.products);
+      },
+      error: err => (this.errorMessage = err)
+    });
   }
 }
