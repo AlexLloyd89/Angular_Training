675cfae24a56a6723564e810d6bfe05787cc93e9
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-16 08:51:26 -0400

    show/hide image on button click

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "pm-root",
+  template: `
+    <div>
+      <div>
+        <h1>{{ title }}</h1>
+        <div>
+          <pm-products></pm-products>
+        </div>
+      </div>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title = "Angular: Getting Started";
 }
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
