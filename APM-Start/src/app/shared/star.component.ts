50449e57403551805dfd0bd6cf421de9fab6a277
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-16 10:25:38 -0400

    nested component communication

---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,26 @@
+import {
+  Component,
+  OnChanges,
+  Input,
+  EventEmitter,
+  Output
+} from "@angular/core";
+
+@Component({
+  selector: "pm-star",
+  templateUrl: "./star.component.html",
+  styleUrls: ["./star.component.css"]
+})
+export class StarComponent implements OnChanges {
+  @Input() rating: number;
+  starWidth: number;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnChanges(): void {
+    this.starWidth = (this.rating * 75) / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`${this.rating} i was clicked`);
+  }
+}
