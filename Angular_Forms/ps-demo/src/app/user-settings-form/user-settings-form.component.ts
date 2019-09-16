613498f7c69a736e17c624f18982b23f5e4c6c5e
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-16 17:36:05 -0400

    form validation

--- Angular_Forms/ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ Angular_Forms/ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserSettings } from "../data/user-settings";
+import { NgForm, NgModel } from "@angular/forms";
 
 @Component({
-  selector: 'app-user-settings-form',
-  templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.css']
+  selector: "app-user-settings-form",
+  templateUrl: "./user-settings-form.component.html",
+  styleUrls: ["./user-settings-form.component.css"]
 })
 export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
+    name: null,
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType: "Annual",
+    notes: "test notes..."
+  };
 
-  constructor() { }
+  userSettings: UserSettings = { ...this.originalUserSettings };
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(form: NgForm) {
+    console.log("on subbmit", form.valid);
   }
 
+  onBlur(field: NgModel) {
+    console.log("onblur", field.valid);
+  }
 }
