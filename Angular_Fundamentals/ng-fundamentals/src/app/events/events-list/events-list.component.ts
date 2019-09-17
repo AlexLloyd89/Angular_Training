b1e56c9debdd7e89d5c1846a0c08fad0eedd2ae8
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-17 11:37:07 -0400

    fix

--- Angular_Fundamentals/ng-fundamentals/src/app/events/events-list/events-list.component.ts
+++ Angular_Fundamentals/ng-fundamentals/src/app/events/events-list/events-list.component.ts
@@ -1,18 +1,17 @@
 import { Component } from "@angular/core";
 import { EventService } from "../../services/events.service";
-import { TestService } from '../../services/test.service';
 
 @Component({
   selector: "events-list",
-  templateUrl: './events-list.component.html',
-  styleUrls: ['./events-list.component.scss']
+  templateUrl: "./events-list.component.html",
+  styleUrls: ["./events-list.component.scss"]
 })
 export class EventsListComponent {
   events: any[];
 
-  constructor(private testService: TestService) {}
+  constructor(private eventService: EventService) {}
 
   ngOnInit() {
-    // this.events = this.eventService.getEvents();
+    this.events = this.eventService.getEvents();
   }
 }
