35587ad8fb4aa057dfd2cf69516e5c29967ec193
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-17 10:12:01 -0400

    some ng stuff

--- Angular_Fundamentals/ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ Angular_Fundamentals/ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -4,20 +4,30 @@ import { Component, Input } from "@angular/core";
   selector: "events-thumbnail",
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date :{{ event.date }}</div>
-      <div>Time :{{ event.time }}</div>
-      <div>Price : \${{ event.price }}</div>
-      <div>
-        <span> Location: {{ event.location.address }}</span>
+      <h2>{{ event?.name }}</h2>
+      <div>Date :{{ event?.date }}</div>
+      <div>Time :{{ event?.time }}</div>
+      <div>Price : \${{ event?.price }}</div>
+      <div *ngIf="event?.location">
+        <span> Location: {{ event?.location?.address }}</span>
 
         <span class="pad-left"
-          >{{ event.location.city }}, {{ event.location.country }}</span
+          >{{ event?.location?.city }}, {{ event?.location?.country }}</span
         >
       </div>
+      <div *ngIf="event?.onlineUrl">Online URL: {{ event?.onlineUrl }}</div>
     </div>
   `,
-  styles: [`.pad-left {margin-leftL 10px;}`]
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .thumbnail {
+        min-height: 210px;
+      }
+    `
+  ]
 })
 export class EventsThumbnailComponent {
   @Input() event: any;
--- Angular_Fundamentals/ng-fundamentals/src/app/nav/navbar.component.ts
+++ Angular_Fundamentals/ng-fundamentals/src/app/nav/navbar.component.ts
@@ -2,6 +2,17 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "nav-bar",
-  templateUrl: "./nav-bar.component.html"
+  templateUrl: "./nav-bar.component.html",
+  styles: [
+    `
+      .nav.navbbar-nav {
+        font-size: 15px;
+      }
+      #searchForm {
+        margin-right: 100px;
+      }
+      @media (max-width: 1200px) (#searchForm {display:none});
+    `
+  ]
 })
 export class NavBarComponent {}
