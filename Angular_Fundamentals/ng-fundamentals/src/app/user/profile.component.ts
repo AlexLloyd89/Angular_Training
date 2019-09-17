a38f743bf40c316536f66fd9b1ea6844b3b7942d
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-17 16:41:48 -0400

    basic reactive form

--- Angular_Fundamentals/ng-fundamentals/src/app/user/profile.component.ts
+++ Angular_Fundamentals/ng-fundamentals/src/app/user/profile.component.ts
@@ -1,31 +1,63 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "./auth.service";
 import { Router } from "@angular/router";
 
 @Component({
-  templateUrl: "./profile.component.html"
+  templateUrl: "./profile.component.html",
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 110px;
+      }
+      .error input {
+        background-color: #e3c3c5;
+      }
+      .error ::-webkit-input-placeholder {
+        color: #999;
+      }
+    `
+  ]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstName);
-    let lastName = new FormControl(this.authService.currentUser.lastName);
+    this.firstName = new FormControl(this.authService.currentUser.firstName, [
+      Validators.required,
+      Validators.pattern("[a-zA-z].*")
+    ]);
+    this.lastName = new FormControl(
+      this.authService.currentUser.lastName,
+      Validators.required
+    );
 
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     });
   }
 
   saveProfile(formValues) {
-    this.authService.updateCurrentUser(
-      formValues.firstName,
-      formValues.lastName
-    );
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(
+        formValues.firstName,
+        formValues.lastName
+      );
+    }
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.touched;
+  }
+  validateFirstName() {
+    return this.firstName.valid || this.firstName.touched;
   }
 
   cancel() {
