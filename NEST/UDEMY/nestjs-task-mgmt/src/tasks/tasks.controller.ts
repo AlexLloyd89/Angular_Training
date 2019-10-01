3dc0c7ffc33c2b00575be0ddca0685c046821fe5
Alex Lloyd <lloyd.da89@gmail.com>
2019-10-01 06:25:14 -0400

    udemy nest

---
+++ NEST/UDEMY/nestjs-task-mgmt/src/tasks/tasks.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete(':id')
+  deleteTaskById(@Param('id') id): void {
+    this.tasksService.deleteTask(id);
+  }
+}
